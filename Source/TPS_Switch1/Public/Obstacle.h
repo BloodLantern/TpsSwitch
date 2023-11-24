ba3918d5f62a1ae34307ea191dd4baacfd261071
Eole TRIER <e.trier@student.isartdigital.com>
2023-11-24 15:55:21 +0100

    Obstacle class and subclass added

---
+++ Source/TPS_Switch1/Public/Obstacle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../EntityBase.h"
+#include "Obstacle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_SWITCH1_API AObstacle : public AEntityBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TPS_Switch1/Public/Obstacles/Chair.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Obstacle.h"
+#include "Chair.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_SWITCH1_API AChair : public AObstacle
+{
+	GENERATED_BODY()
+	
+};
