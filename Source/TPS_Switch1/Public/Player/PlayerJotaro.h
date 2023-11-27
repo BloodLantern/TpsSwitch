4b010711a4b2adb82cc94f8f197cd5da93cb1334
m.grimardhamel <m.grimardhamel@student.isartdigital.com>
2023-11-27 09:54:41 +0100

    Created base player c++ classes

---
+++ Source/TPS_Switch1/Public/Player/PlayerJotaro.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerBase.h"
+#include "PlayerJotaro.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_SWITCH1_API APlayerJotaro : public APlayerBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TPS_Switch1/Public/Player/PlayerStarPlatinum.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerBase.h"
+#include "PlayerStarPlatinum.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_SWITCH1_API APlayerStarPlatinum : public APlayerBase
+{
+	GENERATED_BODY()
+	
+};
