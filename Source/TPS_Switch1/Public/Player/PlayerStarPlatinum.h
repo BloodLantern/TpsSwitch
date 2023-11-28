1d085ff175d5e003efa726910ca34d283b0ec898
Clement TAILLEE <c.taillee@student.isartdigital.com>
2023-11-28 16:03:21 +0100

    fix SP anim and add bullets effects

--- Source/TPS_Switch1/Public/Player/PlayerStarPlatinum.h
+++ Source/TPS_Switch1/Public/Player/PlayerStarPlatinum.h
@@ -2,9 +2,12 @@
 
 #pragma once
 
+#include "GameFramework/Actor.h"
 #include "CoreMinimal.h"
 #include "PlayerBase.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "Player/PlayerJotaro.h"
+#include "Animation/AnimSequence.h"
 #include "PlayerStarPlatinum.generated.h"
 
 /**
@@ -16,6 +19,25 @@ class TPS_SWITCH1_API APlayerStarPlatinum : public APlayerBase
 	GENERATED_BODY()
 
 public:
+	//virtual void BeginPlay() override;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Player)
 	APlayerJotaro* StarPlatinum;
+
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* StarBody = GetComponentByClass<USkeletalMeshComponent>();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool ShouldAim;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool ShouldPunch;
+
+	UFUNCTION(BlueprintCallable)
+	void OnInputPressed();
+
+	UFUNCTION(BlueprintCallable)
+	void OnInputReleased();
+
+	void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent);
 };
